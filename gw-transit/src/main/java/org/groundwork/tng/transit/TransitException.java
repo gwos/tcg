390056b351576bc2a45372e5fd40e2207e7a6073
David S Taylor <david@bluesunrise.com>
2019-10-02 20:29:17 -0700

    GROUNDWORK-489: jump starting the java dev in tng/go project for now

---
+++ gw-transit/src/main/java/org/groundwork/tng/transit/TransitException.java
@@ -0,0 +1,24 @@
+package org.groundwork.tng.transit;
+
+public class TransitException  extends RuntimeException
+{
+    public TransitException()
+    {
+    }
+
+    public TransitException(String message)
+    {
+        super(message);
+    }
+
+    public TransitException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public TransitException(Throwable cause)
+    {
+        super(cause);
+    }
+
+}
---
+++ gw-transit/src/main/java/org/groundwork/tng/transit/TransitServices.java
@@ -0,0 +1,16 @@
+package org.groundwork.tng.transit;
+
+import org.groundwork.rs.dto.DtoOperationResults;
+import org.groundwork.rs.transit.DtoInventory;
+import org.groundwork.rs.transit.DtoMetricDescriptor;
+import org.groundwork.rs.transit.DtoResourceWithMetricsList;
+
+import java.util.List;
+
+public interface TransitServices {
+
+    DtoOperationResults SendResourcesWithMetrics(DtoResourceWithMetricsList resources) throws TransitException;
+    List<DtoMetricDescriptor> ListMetrics() throws TransitException;
+    DtoOperationResults SynchronizeInventory(DtoInventory inventory) throws TransitException;
+
+}
