a19e53b5c1319a54c92b8e90a970fea1ff3fab88
David S Taylor <david@bluesunrise.com>
2019-10-03 21:05:09 -0700

    GROUNDWORK-489: adding starter integration test

--- gw-transit/src/test/java/org/groundwork/tng/transit/AppTest.java
+++ gw-transit/src/test/java/org/groundwork/tng/transit/AppTest.java
@@ -2,7 +2,17 @@ package org.groundwork.tng.transit;
 
 import static org.junit.Assert.assertTrue;
 
+import org.groundwork.rs.dto.DtoOperationResults;
+import org.groundwork.rs.transit.DtoMonitoredResource;
+import org.groundwork.rs.transit.DtoResourceWithMetrics;
+import org.groundwork.rs.transit.DtoResourceWithMetricsList;
+import org.groundwork.rs.transit.DtoTimeSeries;
+import org.groundwork.rs.transit.DtoTracerContext;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -10,11 +20,35 @@ import org.junit.Test;
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     * Test sending resource and metrics
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldSendResourceAndMetrics() {
+        TransitServices transit = new TransitServicesImpl();
+        DtoResourceWithMetricsList resources = new DtoResourceWithMetricsList();
+        // TODO: constructor for tracer context or Builder Pattern
+        DtoTracerContext tracer = new DtoTracerContext();
+        tracer.setAgentId("3939333393342");
+        tracer.setAppType("VEMA");
+        tracer.setTimeStamp(new Date());
+        tracer.setTraceToken("token-99e93");
+        resources.setContext(tracer);
+        // TODO: Builder pattern
+        DtoResourceWithMetrics resourceWithMetrics = new DtoResourceWithMetrics();
+        // TODO: Builder pattern
+        DtoMonitoredResource resource = new DtoMonitoredResource();
+
+        // TODO: builder pattern
+        List<DtoTimeSeries> timeSeries = new ArrayList<>();
+        DtoTimeSeries ts1 = new DtoTimeSeries();
+
+        resourceWithMetrics.setResource(resource);
+        resourceWithMetrics.setMetrics(timeSeries);
+        resources.add(resourceWithMetrics);
+
+        DtoOperationResults results = transit.SendResourcesWithMetrics(resources);
+        assertTrue( results.getCount().equals(1) );
+        assertTrue( results.getSuccessful().equals(1) );
+        assertTrue( results.getFailed().equals(0) );
     }
 }
