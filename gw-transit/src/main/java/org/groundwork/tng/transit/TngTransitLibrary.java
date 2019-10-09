5a56d7b8771491d02d17eb1dd86065f4d0ac3700
Vladislav <vladislav.senkevich@effective-soft.com>
2019-10-09 17:46:01 +0300

    Add nats queue for synchronizer

--- gw-transit/src/main/java/org/groundwork/tng/transit/TngTransitLibrary.java
+++ gw-transit/src/main/java/org/groundwork/tng/transit/TngTransitLibrary.java
@@ -7,7 +7,7 @@ public interface TngTransitLibrary extends Library {
     public String SendMetrics(String metric, StringByReference errorMsg);
     public String ListMetrics(StringByReference errorMsg);
     public boolean SendResourcesWithMetrics(String resourceWithMetricsJson, StringByReference errorMsg);
-    public String SynchronizeInventory(String inventoryJson, StringByReference errorMsg);
+    public boolean SynchronizeInventory(String inventoryJson, StringByReference errorMsg);
     public void ListInventory(StringByReference errorMsg);
     public boolean Connect(String credentialsJson, StringByReference errorMsg);
     public boolean Disconnect(StringByReference errorMsg);
