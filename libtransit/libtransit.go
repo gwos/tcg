3382aa42044da24f3aba43127f1a61eb201d8e5e
Vladislav <vladislav.senkevich@effective-soft.com>
2019-10-10 20:18:11 +0300

    Add vendoring and move all transit properties to config.yml

--- libtransit/libtransit.go
+++ libtransit/libtransit.go
@@ -1,36 +1,15 @@
 package main
 
-// #define ERROR_LEN 250 /* for strncpy error message */
-//include <string.h> /* for strncpy error message */
+//#define ERROR_LEN 250 /* for strncpy error message */
+//#include <string.h> /* for strncpy error message */
 import "C"
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gwos/tng/transit"
-	"log"
 )
 
 var transitService transit.Service
 
 func main() {
-}
-
-//export TestMonitoredResource
-func TestMonitoredResource(str *C.char, errorMsg *C.char) *C.char {
-	resource := transit.MonitoredResource{}
-	if err := json.Unmarshal([]byte(C.GoString(str)), &resource); err != nil {
-		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-		return nil
-	}
-
-	// resource.Labels = map[string]string{"key1": "value1", "key02": "value02"}
-	resource.Status = transit.SERVICE_PENDING
-	buf, _ := json.Marshal(&resource)
-
-	log.Printf("#TestMonitoredResource: %v, %s", resource, buf)
-
-	/* https://github.com/golang/go/wiki/cgo#go-strings-and-c-strings */
-	return C.CString(string(buf))
 }
 
 //export SendResourcesWithMetrics
@@ -44,26 +23,25 @@ func SendResourcesWithMetrics(resourcesWithMetricsJson, errorMsg *C.char) bool {
 	return true
 }
 
-//export ListMetrics
-func ListMetrics(errorMsg *C.char) *C.char {
-	monitorDescriptor, err := transitService.ListMetrics()
-	if err != nil {
-		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-		return nil
-	}
-
-	bytes, err := json.Marshal(monitorDescriptor)
-	if err != nil {
-		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-		return nil
-	}
-
-	return C.CString(string(bytes))
-}
+////export ListMetrics
+//func ListMetrics(errorMsg *C.char) *C.char {
+//	monitorDescriptor, err := transitService.ListMetrics()
+//	if err != nil {
+//		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
+//		return nil
+//	}
+//
+//	bytes, err := json.Marshal(monitorDescriptor)
+//	if err != nil {
+//		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
+//		return nil
+//	}
+//
+//	return C.CString(string(bytes))
+//}
 
 //export SynchronizeInventory
 func SynchronizeInventory(inventoryJson, errorMsg *C.char) bool {
-	fmt.Println(C.GoString(inventoryJson))
 	err := transitService.SynchronizeInventory([]byte(C.GoString(inventoryJson)))
 	if err != nil {
 		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
