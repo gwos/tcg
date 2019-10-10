674b09e0f215c22a0885214c746d5f328782345f
Vladislav <vladislav.senkevich@effective-soft.com>
2019-10-10 16:18:44 +0300

    Move all transit logic to service.go

--- libtransit/libtransit.go
+++ libtransit/libtransit.go
@@ -1,46 +1,18 @@
 package main
 
 // #define ERROR_LEN 250 /* for strncpy error message */
-// #include <string.h> /* for strncpy error message */
+//include <string.h> /* for strncpy error message */
 import "C"
 import (
 	"encoding/json"
-	"github.com/gwos/tng/nats"
+	"fmt"
 	"github.com/gwos/tng/transit"
 	"log"
 )
 
-const (
-	SendResourceWithMetricsSubject = "send-resource-with-metrics"
-	SynchronizeInventorySubject    = "synchronize-inventory"
-)
-
-var transitConfig transit.Transit
+var transitService transit.Service
 
 func main() {
-}
-
-func init() {
-	dispatcherMap := nats.DispatcherMap{
-		SendResourceWithMetricsSubject: func(b []byte) error {
-			_, err := transitConfig.SendResourcesWithMetrics(b)
-			return err
-		},
-		SynchronizeInventorySubject: func(b []byte) error {
-			_, err := transitConfig.SynchronizeInventory(b)
-			return err
-		},
-	}
-
-	_, err := nats.StartServer()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = nats.StartDispatcher(&dispatcherMap)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 //export TestMonitoredResource
@@ -63,39 +35,18 @@ func TestMonitoredResource(str *C.char, errorMsg *C.char) *C.char {
 
 //export SendResourcesWithMetrics
 func SendResourcesWithMetrics(resourcesWithMetricsJson, errorMsg *C.char) bool {
-	//var resourceWithMetrics transit.SendMetricsRequest
-
-	//err := json.Unmarshal([]byte(C.GoString(resourcesWithMetricsJson)), &resourceWithMetrics)
-	//if err != nil {
-	//	C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-	//	return nil
-	//}
-
-	//operationResults, err := transitConfig.SendResourcesWithMetrics(&resourceWithMetrics)
-	//if err != nil {
-	//	C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-	//	return nil
-	//}
-
-	err := nats.Publish(SendResourceWithMetricsSubject, []byte(C.GoString(resourcesWithMetricsJson)))
+	err := transitService.SendResourceWithMetrics([]byte(C.GoString(resourcesWithMetricsJson)))
 	if err != nil {
 		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
 		return false
 	}
 
 	return true
-
-	//operationResultsJson, err := json.Marshal(operationResults)
-	//if err != nil {
-	//	C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-	//	return nil
-	//}
-	//return C.CString(string(operationResultsJson))
 }
 
 //export ListMetrics
 func ListMetrics(errorMsg *C.char) *C.char {
-	monitorDescriptor, err := transitConfig.ListMetrics()
+	monitorDescriptor, err := transitService.ListMetrics()
 	if err != nil {
 		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
 		return nil
@@ -112,71 +63,16 @@ func ListMetrics(errorMsg *C.char) *C.char {
 
 //export SynchronizeInventory
 func SynchronizeInventory(inventoryJson, errorMsg *C.char) bool {
-	//fmt.Println(C.GoString(inventoryJson))
-
-	//var inventory transit.SendInventoryRequest
-	//
-	//err := json.Unmarshal([]byte(C.GoString(inventoryJson)), &inventory)
-	//if err != nil {
-	//	C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-	//	return nil
-	//}
-
-	err := nats.Publish(SynchronizeInventorySubject, []byte(C.GoString(inventoryJson)))
+	fmt.Println(C.GoString(inventoryJson))
+	err := transitService.SynchronizeInventory([]byte(C.GoString(inventoryJson)))
 	if err != nil {
 		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
 		return false
 	}
 
 	return true
-
-	//if err != nil {
-	//	C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-	//	return nil
-	//}
-	//
-	//operationResultsJson, err := json.Marshal(operationResults)
-	//if err != nil {
-	//	C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-	//	return nil
-	//}
-	//
-	//return C.CString(string(operationResultsJson))
 }
 
 //TODO:
 func ListInventory() {
-}
-
-//export Connect
-func Connect(credentialsJson, errorMsg *C.char) bool {
-	transitConfig = transit.Transit{}
-
-	var credentials transit.Credentials
-
-	err := json.Unmarshal([]byte(C.GoString(credentialsJson)), &credentials)
-	if err != nil {
-		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-		return false
-	}
-
-	err = transitConfig.Connect(credentials)
-	if err != nil {
-		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-		return false
-	}
-
-	//start nats
-
-	return true
-}
-
-//export Disconnect
-func Disconnect(errorMsg *C.char) bool {
-	err := transitConfig.Disconnect()
-	if err != nil {
-		C.strncpy((*C.char)(errorMsg), C.CString(err.Error()), C.ERROR_LEN)
-		return false
-	}
-	return true
 }
