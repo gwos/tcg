5b4c21da5829d65d0ebefb347eb78f11a1125940
Vladislav <vladislav.senkevich@effective-soft.com>
2019-10-16 16:31:52 +0300

    Add functions to control NATS streaming. Edit Readme

--- gw-transit/src/main/java/org/groundwork/tng/transit/TngTransitLibrary.java
+++ gw-transit/src/main/java/org/groundwork/tng/transit/TngTransitLibrary.java
@@ -4,10 +4,12 @@ package org.groundwork.tng.transit;
 import com.sun.jna.Library;
 
 public interface TngTransitLibrary extends Library {
-    public String SendMetrics(String metric, StringByReference errorMsg);
     public String ListMetrics(StringByReference errorMsg);
     public boolean SendResourcesWithMetrics(String resourceWithMetricsJson, StringByReference errorMsg);
     public boolean SynchronizeInventory(String inventoryJson, StringByReference errorMsg);
     public void ListInventory(StringByReference errorMsg);
-    public boolean Disconnect(StringByReference errorMsg);
+    public boolean StartNATS(StringByReference errorMsg);
+    public void StopNATS();
+    public boolean StartTransport(StringByReference errorMsg);
+    public boolean StopTransport(StringByReference errorMsg);
 }
