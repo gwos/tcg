cbe6925b0925e3da5e70adadd5fd734913251366
Pavlo Sumkin <ymkins@gmail.com>
2019-11-12 19:59:01 +0200

    Fix integration tests

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -2,21 +2,21 @@ package integration
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
+	"github.com/gwos/tng/clients"
 	"github.com/gwos/tng/services"
-	"github.com/gwos/tng/transit"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path"
+	"reflect"
 	"testing"
 )
 
 const (
-	HostGetApi        = "http://localhost/api/hosts/"
-	HostDeleteApi     = "http://localhost/api/hosts/"
+	HostGetAPI        = "http://localhost/api/hosts/"
+	HostDeleteAPI     = "http://localhost/api/hosts/"
 	HostStatusPending = "PENDING"
 	HostStatusUp      = "UP"
 	TestHostName      = "GW8_TNG_TEST_HOST"
@@ -73,17 +73,18 @@ func config() (map[string]string, error) {
 		return nil, err
 	}
 
+	token := reflect.ValueOf(service).Elem().FieldByName("token").String()
 	headers := map[string]string{
 		"Accept":         "application/json",
 		"GWOS-APP-NAME":  service.GroundworkConfig.AppName,
-		"GWOS-API-TOKEN": service.GroundworkConfig.Token,
+		"GWOS-API-TOKEN": token,
 	}
 
 	return headers, nil
 }
 
 func existenceCheck(mustExist bool, mustHasStatus string) error {
-	statusCode, byteResponse, err := transit.SendRequest(http.MethodGet, HostGetApi+TestHostName, headers, nil, nil)
+	statusCode, byteResponse, err := clients.SendRequest(http.MethodGet, HostGetAPI+TestHostName, headers, nil, nil)
 	if err != nil {
 		return err
 	}
@@ -91,7 +92,7 @@ func existenceCheck(mustExist bool, mustHasStatus string) error {
 		return nil
 	}
 	if !(!mustExist && statusCode != 404) {
-		return errors.New(fmt.Sprintf("Status code = %d(Details: %s), want = %d", statusCode, string(byteResponse), 404))
+		return fmt.Errorf("Status code = %d(Details: %s), want = %d", statusCode, string(byteResponse), 404)
 	}
 
 	var response Response
@@ -102,8 +103,8 @@ func existenceCheck(mustExist bool, mustHasStatus string) error {
 	}
 
 	if mustExist && (response.HostName != TestHostName || response.MonitorStatus != mustHasStatus) {
-		return errors.New(fmt.Sprintf("Host from database = (Name: %s, Status: %s), want = (Name: %s, Status: %s)",
-			response.HostName, response.MonitorStatus, TestHostName, mustHasStatus))
+		return fmt.Errorf("Host from database = (Name: %s, Status: %s), want = (Name: %s, Status: %s)",
+			response.HostName, response.MonitorStatus, TestHostName, mustHasStatus)
 	}
 
 	return nil
@@ -142,7 +143,7 @@ func runJavaSendResourceWithMetricsTest() error {
 }
 
 func clean(headers map[string]string) {
-	_, _, err := transit.SendRequest(http.MethodDelete, HostDeleteApi+TestHostName, headers, nil, nil)
+	_, _, err := clients.SendRequest(http.MethodDelete, HostDeleteAPI+TestHostName, headers, nil, nil)
 	if err != nil {
 		log.Println(err)
 	}
