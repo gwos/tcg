775e45b29e2ef7a68c63e5e7af45ceb21f32da7c
Glenn Herteg <gherteg@digit7.groundwork.groundworkopensource.com>
2019-11-26 07:38:46 -0800

    GROUNDWORK-491-serialization-wip:  substantial cleanup of code and doc
    This commit includes extending the documentation to describe how
    an application is expected to use the generated JSON conversion
    routines.  The conversion tool itself has been modified to suppress
    diagnostic output unless explicitly requested, and to provide
    user control over where the generated .h and .c files are placed.
    The tool Makefile also now supports testing of independent files
    containing JSON strings representing objects in the test Go code.
    The latter facility will allow more extensive testing with far less
    manual code editing.

---
+++ gotocjson/transit_main.c
@@ -0,0 +1,17 @@
+// Sample application.
+// Here just to illustrate how such an application is linked.
+
+// Required for any application using the output of this tool.
+#include "convert_go_to_c.h"
+
+// Required on a per-package basis, for using any of the declarations
+// and definitions provided in the respective header files.
+#include "config.h"
+#include "milliseconds.h"
+#include "transit.h"
+
+// Dummy main program, just to get this sample program to compile.
+int main(int argc, char *argv[])
+    {
+    return 0;
+    }
