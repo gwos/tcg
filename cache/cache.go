234e5ef35e8c5ed8a713162406e4a098f3e87a9e
Pavlo Sumkin <pavlo@bluesunrise.com>
2020-02-12 16:10:59 +0200

    GROUNDWORK-806 Reduce entropy consumption on TracerToken

--- cache/cache.go
+++ cache/cache.go
@@ -5,10 +5,16 @@ import (
 	"time"
 )
 
+// AuthCache used by Controller.validateToken
 var AuthCache = cache.New(8*time.Hour, time.Hour)
 
+// NatsCache used by NATS Dispatcher
 var NatsCache = cache.New(10*time.Minute, 10*time.Minute)
 
+// TraceTokenCache used by AgentService
+var TraceTokenCache = cache.New(-1, -1)
+
+// Credentials defines type of AuthCache items
 type Credentials struct {
 	GwosAppName  string
 	GwosAPIToken string
