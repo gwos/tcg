8d50262ca237c2614bcd898d40ffc8bf39f2d6b1
Pavlo Sumkin <pavlo@bluesunrise.com>
2020-03-11 21:20:41 +0200

    GROUNDWORK-852 Enhance DemandConfig

--- services/agentService_test.go
+++ services/agentService_test.go
@@ -2,20 +2,20 @@ package services
 
 import (
 	"github.com/gwos/tng/config"
-	"github.com/gwos/tng/log"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func init() {
-	log.Error("HEAREEEE")
 	config.GetConfig().Connector.NatsStoreType = "MEMORY"
 	config.GetConfig().GWConnections = []*config.GWConnection{
 		{
+			Enabled:  true,
 			HostName: "test",
 			UserName: "test",
 			Password: "test",
-			Enabled: true,
 		},
 	}
 }
@@ -36,4 +36,37 @@ func TestAgentService_StartStopTransport(t *testing.T) {
 	assert.NoError(t, GetAgentService().StopTransport())
 	assert.NoError(t, GetAgentService().StartTransport())
 	assert.NoError(t, GetAgentService().StopNats())
+}
+
+func TestAgentService_DemandConfig(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "config")
+	assert.NoError(t, err)
+	err = tmpfile.Close()
+	assert.NoError(t, err)
+	defer os.Remove(tmpfile.Name())
+	os.Setenv(string(config.ConfigEnv), tmpfile.Name())
+
+	dto := []byte(`
+{
+  "agentId": "99998888-7777-6666-a3b0-b14622f7dd39",
+  "appName": "test-app-XX",
+  "appType": "test-XX",
+  "logLevel": 2,
+  "tngUrl": "http://tng-host:9980/",
+  "dalekservicesConnection": {
+    "hostName": "gw-host-xxx"
+  },
+  "groundworkConnections": [{
+    "enabled": true,
+    "hostName": "gw-host-xx",
+    "userName": "-xx-",
+    "password": "xx"
+  }]
+}`)
+
+	agentService := GetAgentService()
+	assert.Equal(t, "", agentService.Connector.AgentID)
+	agentService.config(dto)
+	assert.Equal(t, "99998888-7777-6666-a3b0-b14622f7dd39", agentService.Connector.AgentID)
+	assert.Equal(t, "gw-host-xxx", agentService.DSClient.HostName)
 }
