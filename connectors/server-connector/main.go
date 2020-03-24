5d5a6eeba9509904aaa0ad30d98070ec83eeb15d
David S Taylor <david@bluesunrise.com>
2020-03-23 20:45:24 -0700

    GROUNDWORK-859: grab ownershiptype from config and pass to foundation

--- connectors/server-connector/main.go
+++ connectors/server-connector/main.go
@@ -17,6 +17,14 @@ const (
 	DefaultTimer         = 120
 )
 
+type InitializeConfigResult struct {
+	processes []string
+	groups []transit.ResourceGroup
+	timer int
+	ownership transit.HostOwnershipType
+}
+
+
 // @title TNG API Documentation
 // @version 1.0
 
@@ -26,22 +34,15 @@ func main() {
 	var transitService = services.GetTransitService()
 
 	chanel := make(chan bool)
-
-	timer := DefaultTimer
-	var processes []string
-	var groups []transit.ResourceGroup
-
+	var config *InitializeConfigResult
 	transitService.ConfigHandler = func(data []byte) {
-		if p, g, t, err := initializeConfig(data); err == nil {
-			processes = p
-			groups = g
-			timer = t
-			chanel <- true
-		} else {
+		cfg, err := initializeConfig(data)
+		if err != nil {
 			return
 		}
+		config = cfg
+		chanel <- true
 	}
-
 	if err := transitService.DemandConfig(); err != nil {
 		log.Error(err)
 		return
@@ -61,54 +62,70 @@ func main() {
 		if transitService.Status().Transport != services.Stopped {
 			log.Info("TNG ServerConnector: sending inventory ...")
 			// TODO: ownership type
-			_ = connectors.SendInventory([]transit.InventoryResource{*Synchronize(processes)}, groups, transit.Creator)
+			_ = connectors.SendInventory([]transit.InventoryResource{*Synchronize(config.processes)}, config.groups, config.ownership)
 		} else {
 			log.Info("TNG ServerConnector is stopped ...")
 		}
 		for i := 0; i < 10; i++ {
 			if transitService.Status().Transport != services.Stopped {
 				log.Info("TNG ServerConnector: monitoring resources ...")
-				err := connectors.SendMetrics([]transit.MonitoredResource{*CollectMetrics(processes, time.Duration(timer))})
+				err := connectors.SendMetrics([]transit.MonitoredResource{*CollectMetrics(config.processes, time.Duration(config.	timer))})
 				if err != nil {
 					log.Error(err.Error())
 				}
 			}
 			LastCheck = milliseconds.MillisecondTimestamp{Time: time.Now()}
-			time.Sleep(time.Duration(int64(timer) * int64(time.Second)))
+			time.Sleep(time.Duration(int64(config.timer) * int64(time.Second)))
 		}
 	}
 }
 
-func initializeConfig(data []byte) ([]string, []transit.ResourceGroup, int, error) {
+func initializeConfig(data []byte) (*InitializeConfigResult, error) {
+	// just grab the fields that are needed by this connector
 	var connector = struct {
 		Connection transit.MonitorConnection `json:"monitorConnection"`
+		Connections []struct {
+			DeferOwnership transit.HostOwnershipType	`json:"deferOwnership"`
+		} `json:"groundworkConnections"`
 	}{}
-
+	config := InitializeConfigResult{
+		processes:   []string{},
+		groups: []transit.ResourceGroup{},
+		timer: 0,
+		ownership: transit.Yield,
+	}
 	err := json.Unmarshal(data, &connector)
 	if err != nil {
-		return []string{}, []transit.ResourceGroup{}, -1, err
+		return &InitializeConfigResult{
+			processes:   []string{},
+			groups: []transit.ResourceGroup{},
+			timer: 0,
+			ownership: transit.Yield,
+		}, err
 	}
 	timer := float64(DefaultTimer)
 	if _, present := connector.Connection.Extensions["timer"]; present {
 		timer = connector.Connection.Extensions["timer"].(float64)
 	}
-	var processes []string
+	config.timer = int(timer)
+
 	if _, present := connector.Connection.Extensions["processes"]; present {
 		processesInterface := connector.Connection.Extensions["processes"].([]interface{})
 		for _, process := range processesInterface {
-			processes = append(processes, process.(string))
+			config.processes = append(config.processes, process.(string))
 		}
 	}
-	var groups []transit.ResourceGroup
 	if _, present := connector.Connection.Extensions["groups"]; present {
 		groupsInterface := connector.Connection.Extensions["groups"].([]interface{})
 		for _, gr := range groupsInterface {
 			groupMap := gr.(map[string]interface{})
-			groups = append(groups, transit.ResourceGroup{GroupName: groupMap["name"].(string), Type: transit.GroupType(groupMap["type"].(string))})
+			config.groups = append(config.groups, transit.ResourceGroup{GroupName: groupMap["name"].(string), Type: transit.GroupType(groupMap["type"].(string))})
 		}
 	} else {
-		groups = append(groups, transit.ResourceGroup{GroupName: DefaultHostGroupName, Type: transit.HostGroup})
+		config.groups = append(config.groups, transit.ResourceGroup{GroupName: DefaultHostGroupName, Type: transit.HostGroup})
 	}
-
-	return processes, groups, int(timer), nil
+	if len(connector.Connections) > 0 {
+		config.ownership = connector.Connections[0].DeferOwnership
+	}
+	return &config, nil
 }
