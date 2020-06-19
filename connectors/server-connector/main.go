f48ae863c492044df883b76da587b51f63b65a5e
David S Taylor <david@bluesunrise.com>
2020-06-19 16:31:53 -0700

    HOTFIX: yet another tight loop

--- connectors/server-connector/main.go
+++ connectors/server-connector/main.go
@@ -93,8 +93,9 @@ func main() {
 					log.Error(err.Error())
 				}
 			}
-			time.Sleep(time.Duration(connectors.Timer * int64(time.Second)))
 		}
+		log.Debug("sleeping for ...", connectors.Timer)
+		time.Sleep(time.Duration(connectors.Max(connectors.Timer, 60) * int64(time.Second)))
 	}
 }
 
