4088fe9c60f670427a8b4d060bb6d488872a66ed
VladislavSenkevich <vladislav.senkevich@effective-soft.com>
2020-07-21 15:44:40 +0300

    Fix NPE and improve Java example

--- examples/java/src/main/java/org/example/App.java
+++ examples/java/src/main/java/org/example/App.java
@@ -1,46 +1,13 @@
 package org.example;
 
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
 
 @SpringBootApplication
 @EnableScheduling
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    private BeerService beerService;
-
-    public App(BeerService beerService) {
-        this.beerService = beerService;
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void orderBeers() {
-        Flux.interval(Duration.ofSeconds(2))
-                .map(App::toOrder)
-                .doOnEach(o -> beerService.orderBeer(o.get()))
-                .subscribe();
-    }
-
-    private static Order toOrder(Long l) {
-        long amount = l % 5;
-        String type = l % 2 == 0 ? "ale" : "light";
-        return new Order((int) amount, type);
-    }
-
-    @Bean
-    public TimedAspect timedAspect(MeterRegistry registry) {
-        return new TimedAspect(registry);
     }
 }
--- examples/java/src/main/java/org/example/Controller.java
+++ examples/java/src/main/java/org/example/Controller.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import io.prometheus.client.exporter.common.TextFormat;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +13,29 @@ import java.io.Writer;
 
 @RestController
 public class Controller {
-
-    public static double metricValue = 123;
-
     @RequestMapping("/")
     public String index() throws IOException {
         Writer writer = new StringWriter();
         CollectorRegistry registry = new CollectorRegistry();
-        Gauge gauge = io.prometheus.client.Gauge.build()
-                .name("java_service_example")
-                .help("Description for example").register(registry);
 
-        gauge.set(metricValue);
+        Gauge gauge = io.prometheus.client.Gauge.build()
+                .name("java_gauge_example")
+                .help("Description for gauge")
+                .labelNames("critical", "warning", "resource", "group", "unitType")
+                .register(registry);
+        Gauge.Child gaugeChild = gauge.labels(String.valueOf(Math.random() * 10),
+                String.valueOf(Math.random() * 10), "java-server", "Prometheus-Java", "MB");
+        gaugeChild.set(Math.random() * 10);
+
+        Counter counter = io.prometheus.client.Counter.build()
+                .name("java_counter_example")
+                .help("Description for counter")
+                .labelNames("critical", "warning", "resource", "group", "unitType")
+                .register(registry);
+        Counter.Child counterChild = counter.labels(String.valueOf(Math.random() * 10),
+                String.valueOf(Math.random() * 10), "java-server", "Prometheus-Java", "MB");
+        counterChild.inc(Math.random() * 10);
+
 
         TextFormat.write004(writer, registry.metricFamilySamples());
         return writer.toString();
