b0a800d64c08a0047c7941ba48e3a2d1e0b4dcb0
Pavlo Sumkin <pavlo@bluesunrise.com>
2020-10-06 17:55:23 +0300

    GROUNDWORK-1193 Provide docker image for TCG connectors

---
+++ docker_cmd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+connector=$1
+fs_app=/app
+fs_var=/tcg
+
+if ! [ -x "${fs_app}/${connector}/${connector}" ]; then
+    echo "CONNECTOR NOT FOUND: ${fs_app}/${connector}/${connector}"
+    exit 1
+fi
+
+if ! [ -f "${fs_var}/${connector}/tcg_config.yaml" ]; then
+    mkdir -p "${fs_var}/${connector}"
+    cp "${fs_app}/${connector}/tcg_config.yaml" "${fs_var}/${connector}/tcg_config.yaml" || exit 1
+fi
+
+cd "${fs_var}/${connector}" || exit 1
+"${fs_app}/${connector}/${connector}"
