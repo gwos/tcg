1b18397c5eaed5dd34ff51c349247731455872ce
Pavlo Sumkin <pavlo@bluesunrise.com>
2020-10-15 16:37:06 +0300

    GROUNDWORK-1193 Cleanup C-API tests

--- transit-c/send_config_script.sh
+++ transit-c/send_config_script.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-API_USER='RESTAPIACCESS'
-API_URI='http://c1.bluesunrise.com/api'
-CONFIG_API_URI='http://localhost:8099/api/v1/config'
-API_PASSWORD='63c5BtYDNAPANvNqAkh9quYszwVrvLaruxmzvM4P1FSw'
-#API_PASSWORD='6d2Ygwsw6dM8abSiGCaFvTyWXT8JP8XmuvwX4yynt5TH'
+TCG_API_CONFIG=${TCG_API_CONFIG:-'http://localhost:8099/api/v1/config'}
+GWOS_API_LOGIN=${GWOS_API_LOGIN:-'http://localhost:8080/api/auth/login'}
+GWOS_WSUSERNAME=${GWOS_WSUSERNAME:-'RESTAPIACCESS'}
+GWOS_WSPASSWORD=${GWOS_WSPASSWORD}
 
-TOKEN=$(curl -s -X POST -d "user=${API_USER}&password=${API_PASSWORD}&gwos-app-name=gw8" "${API_URI}/auth/login")
+TOKEN=$(curl -s -X POST -d "user=${GWOS_WSUSERNAME}&password=${GWOS_WSPASSWORD}&gwos-app-name=gw8" "${GWOS_API_LOGIN}")
 if [ $? != 0 ]; then
   echo "ERROR: No auth token"
   exit 1
-else
-  _=$(curl -s -X POST -d '{}' -H "GWOS-APP-NAME:gw8" -H "GWOS-API-TOKEN:${TOKEN}" -H 'Accept: application/json' ${CONFIG_API_URI})
 fi
+
+curl -s -X POST -d '{}' -H "GWOS-APP-NAME:gw8" -H "GWOS-API-TOKEN:${TOKEN}" -H 'Accept: application/json' ${TCG_API_CONFIG}
