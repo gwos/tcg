ddeaa67a31e03bd0fc72a7ca18048e82699ea569
Pavlo Sumkin <pavlo@bluesunrise.com>
2021-08-06 11:44:01 +0300

    GROUNDWORK-1690 Process metrics with parser

--- connectors/checker-connector/nsca/nsca.go
+++ connectors/checker-connector/nsca/nsca.go
@@ -6,13 +6,31 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/gwos/tcg/connectors/checker-connector/parser"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/tubemogul/nscatools"
 )
 
 func Handler(p *nscatools.DataPacket) error {
+	// It's unclear for now how to process multi-line metrics in the right way.
+	// The nscatools.DataPacket provides host, service, and state info for the 1st line only.
+	// And PluginOutput contains the rest.
+	// The `fullPacket` payload that processed in the HandleClient() by nscatools.DataPacket.Read()
+	// contains a lot of noise and looks like protocol defined structure.
+	// So, here we try to reconstruct plain metrics payload and process it with own parser.
+	s := strings.Replace(p.PluginOutput, `\n`, "\n", -1)
+	buf := make([]byte, 0, 4+len(p.HostName)+len(p.Service)+len(s))
+	buf = append(buf, p.HostName...)
+	buf = append(buf, ';')
+	buf = append(buf, p.Service...)
+	buf = append(buf, ';')
+	buf = strconv.AppendInt(buf, int64(p.State), 10)
+	buf = append(buf, ';')
+	buf = append(buf, s...)
 	log.Debug().
 		Int16("version", p.Version).
 		Uint32("crc", p.Crc).
@@ -21,9 +39,20 @@ func Handler(p *nscatools.DataPacket) error {
 		Str("hostname", p.HostName).
 		Str("service", p.Service).
 		Str("pluginOutput", p.PluginOutput).
+		Bytes("buf", buf).
+		Func(func(e *zerolog.Event) {
+			println("##plain")
+			println(string(buf))
+		}).
 		Msg("processing DataPacket")
 
-	return nil
+	_, err := parser.ProcessMetrics(context.Background(), buf, parser.NSCA)
+	if err != nil {
+		log.Warn().Err(err).
+			Bytes("buf", buf).
+			Msg("could not process metrics")
+	}
+	return err
 }
 
 func Start(ctx context.Context) {
