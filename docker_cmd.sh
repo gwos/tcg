d8936830a4b0970ae0c1d71b96efcd3e7d871260
Pavlo Sumkin <pavlo@bluesunrise.com>
2023-06-23 16:50:06 +0300

    GROUNDWORK-2471 update cacerts on starting container

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -8,6 +8,17 @@ for CMD in $(compgen -v ENTRYPOINT_CMD); do
     fi
 done
 
+# update cacerts
+if ls /usr/local/groundwork/config/cacerts/*.pem > /dev/null 2>&1; then
+    for CACERT in /usr/local/groundwork/config/cacerts/*.pem ; do
+        CACERT_FILENAME=$(basename $CACERT)
+        CACERT_NAME=${CACERT_FILENAME%.pem}
+        echo "Update CA certs: adding $CACERT_FILENAME"
+        openssl x509 -in $CACERT -noout -text
+        cp $CACERT /usr/local/share/ca-certificates/${CACERT_NAME}.crt
+    done
+    update-ca-certificates
+fi
 
 connector=$1
 fs_app=/app
